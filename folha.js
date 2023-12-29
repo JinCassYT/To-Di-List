e5a8be93423981c2836c1bc9bc9f392568c8d9c3
Cass <douglaszatara1234@gmail.com>
2023-12-29 20:43:30 -0300

    Limited List

--- folha.js
+++ folha.js
@@ -1,15 +1,22 @@
 const entrada = document.getElementById("entrada");
 const listcontainer = document.getElementById("list-container");
+const limit = 10;
 
 function add(){
     if(entrada.value === ''){
         alert("Digite uma tarefa");
     }
     else{
+        if(listcontainer.children.length < limit){
 
-        let li = document.createElement("li");
-        li.innerHTML = entrada.value;
-        listcontainer.appendChild(li);
+            
+            let li = document.createElement("li");
+            li.innerHTML = entrada.value;
+            listcontainer.appendChild(li);
+        } 
+        else{
+            alert("Limite de tarefas não concluidas atingido");
+        }
 
         
     }
