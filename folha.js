1f39efed81adce7effdb8ec85ff58653fa5cafd2
Cass <douglaszatara1234@gmail.com>
2023-12-29 21:08:07 -0300

    atualização de layout e efetivação de botão de remoção

--- folha.js
+++ folha.js
@@ -13,6 +13,13 @@ function add(){
             let li = document.createElement("li");
             li.innerHTML = entrada.value;
             listcontainer.appendChild(li);
+
+            //botão de excluir 
+            let excluir = document.createElement("button");
+            excluir.innerHTML = "Remover";
+            excluir.onclick = function(){
+            listcontainer.removeChild(li);}
+            li.appendChild(excluir);
         } 
         else{
             alert("Limite de tarefas não concluidas atingido");
